8aed0eb3c7e3e33ecf1a77315f24f63024032ccf
dariusmortazavi <dariusmortazavi@gmail.com>
2020-03-26 14:52:29 -0500

    Test site
    Testing

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Blue lizards taste good, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Blue lizards taste good, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
